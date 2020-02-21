2f26ebb6b612b02e9b198bffa0efbe51bfd14881
Robert Williams <robert@a3marketing.com>
2020-02-21 14:42:33 -0600

    created functions to generate random numbers, letters, and symbols

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,41 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+var confirmPassword = confirm ("Need a secure password?")
+
+
+// Generator Functions
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*()_+<>?\|"
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
+
+console.log(getRandomLower());
+console.log(getRandomUpper());
+console.log(getRandomNumber());
+console.log(getRandomSymbol());
