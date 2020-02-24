bc2918ac33efd8a5d7cfbe487c085c51e0e0cbe8
Robert Williams <robert@a3marketing.com>
2020-02-24 13:16:09 -0600

    fixed issue with prompts popping up before generate button was clicked

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var confirmPassword = confirm ("Need a secure password?");
+  var promptLength = prompt ("How many characters do you want it to be?");
+  var confirmLower = confirm ("Do you want lower case letters?");
+  var confirmUpper = confirm ("Do you want upper case letters?");
+  var confirmSymbols = confirm ("Do you want symbols?");
+  var confirmNumbers = confirm ("Do you want numbers?");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -11,52 +17,38 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
 
 
 
-
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
-
-var confirmPassword = confirm ("Need a secure password?");
-var promptLength = prompt ("How many characters do you want it to be?");
-var confirmLower = confirm ("Do you want lower case letters?");
-var confirmUpper = confirm ("Do you want upper case letters?");
-var confirmSymbols = confirm ("Do you want symbols?");
-var confirmNumbers = confirm ("Do you want numbers?");
 
 
 
 // Generator Functions
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+// function getRandomLower() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
 
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
+// function getRandomUpper() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// }
 
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+// function getRandomNumber() {
+//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+// }
 
-function getRandomSymbol() {
-  const symbols = "!@#$%^&*()_+<>?\|"
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
+// function getRandomSymbol() {
+//   const symbols = "!@#$%^&*()_+<>?\|"
+//   return symbols[Math.floor(Math.random() * symbols.length)];
+// }
 
 
-console.log(promptLength);
-console.log(promptLower);
-console.log(promptNumbers);
-console.log(promptUpper);
-console.log(promptSymbols);
-console.log(getRandomLower());
-console.log(getRandomUpper());
-console.log(getRandomNumber());
-console.log(getRandomSymbol());
+// console.log(promptLength);
+// console.log(promptLower);
+// console.log(promptNumbers);
+// console.log(promptUpper);
+// console.log(promptSymbols);
+// console.log(getRandomLower());
+// console.log(getRandomUpper());
+// console.log(getRandomNumber());
+// console.log(getRandomSymbol());
