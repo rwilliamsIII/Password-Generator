1f0d160a07d9bbb57d6ae16ce190aec20b72a61d
Robert Williams <robert@a3marketing.com>
2020-02-25 18:35:37 -0600

    added all possible characters in to one variable

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lower = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
-var upper = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
-var num = "0,1,2,3,4,5,6,7,8,9";
-var sym = "!@#$%^&*+=?";
+var allChar = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,0,1,2,3,4,5,6,7,8,9,!,@,#,$,%,^,&,*,+,=?";
+var sliceChar = ""
+var finalPass = ""
 // Write password to the #password input
 function writePassword() {
 
@@ -11,26 +10,12 @@ var promptLength = prompt ("How many characters do you want it to be?");
   if (promptLength < 8)
   alert ("Password must be atleast 8 characters long");
   else {};
-var confirmLower = confirm ("Do you want lower case letters?");
-  if (confirmLower === true)
-  ;
-  else {}
-  ;
+var confirmLower = confirm ("Do you want lower case letters?");  
 var confirmUpper = confirm ("Do you want upper case letters?");
-  if (confirmUpper === true)
-  ;
-  else {}
-  ; 
 var confirmSymbols = confirm ("Do you want symbols?");
-  if (confirmSymbols === true)
-  ;
-  else{}
-  ;
 var confirmNumbers = confirm ("Do you want numbers?");
-  if (confirmNumbers === true)
-  ;
-  else {}
-  ;
+  
+  
   var password = generatePassword();
 
   function generatePassword(promptLength){
